1609d4e2fee12dff839b7a4f512c6c6088ee691d
Carlton Joseph <carlton.joseph@gmail.com>
2025-09-27 10:39:45 -0700

    update format

--- src/pages/_trucks/trucks.tsx
+++ src/pages/_trucks/trucks.tsx
@@ -8,37 +8,85 @@ const Trucks = ({ data }: { data: any }) => {
         Truck Options On {data.date} at {data.time}
       </h2>
       <table className="table-auto ml-2 mt-5">
-        {trucks.map((truck: any, idx: number) => {
-          return (
-            <tr
-              key={idx}
-              className="odd:bg-slate-100 even:bg-slate-200 border-y border-gray-300"
-            >
-              <td className="px-2 py-2 text-xs">{idx}</td>
-              <td className="px-2 py-2 text-xs">
-                <a href={`/windowStickers/${truck.vin}.pdf`}>{truck.vin}</a>
-              </td>
-              <td className="px-2 py-2 text-xs">{truck.stickerDate}</td>
-              <td className="px-2 py-2 text-xs">{truck.dealerShort}</td>
-              <td className="px-2 py-2 text-sm">
-                {truck.listPrice + truck.downPayment}
-              </td>
-              <td className="px-2 py-2 text-xs">{truck.exteriorColorShort}</td>
-              <td className="px-2 py-2 flex gap-3 text-xs">
-                {truck.options.manufacture.map(
-                  ({ name }: { name: any }, key: any) => (
-                    <span
-                      key={key}
-                      className="bg-slate-50 px-2 py-1 rounded-lg"
-                    >
-                      {name}
-                    </span>
-                  ),
-                )}
-              </td>
-            </tr>
-          );
-        })}
+        <thead className="text-sm">
+          <tr className="py-2 bg-slate-300">
+            <th className="py-2">VIN, Sticker, Dlr</th>
+            <th className="py-2">Price</th>
+            <th className="py-2">Mfg Options</th>
+            <th className="py-2">Dlr Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trucks.map((truck: any, idx: number) => {
+            return (
+              <tr
+                key={idx}
+                className="odd:bg-slate-100 even:bg-slate-200 border-y border-gray-300"
+              >
+                <td className="px-2 py-2 text-xs align-top">
+                  <div className="flex flex-col items-end">
+                    <a href={`/windowStickers/${truck.vin}.pdf`}>
+                      <div>{truck.vin}</div>
+                    </a>
+                    <div>{truck.stickerDate}</div>
+                    <div>{truck.exteriorColorShort}</div>
+                    <div>{truck.dealerShort}</div>
+                  </div>
+                </td>
+                <td className="px-2 py-2 text-sm font-mono w-[120px]">
+                  <div className="flex justify-between items-end w-full">
+                    <span className="text-xs text-slate-400">bsrp</span>
+                    <span>{truck.bsrp}</span>
+                  </div>
+                  <div className="flex justify-between items-end w-full">
+                    <span className="text-xs text-slate-400">srp</span>
+                    <span>{truck.srp}</span>
+                  </div>
+                  <div className="flex justify-between items-end w-full">
+                    <span className="text-xs text-slate-400">list</span>
+                    <span>{truck.listPrice}</span>
+                  </div>
+                  <div className="flex justify-between items-end w-full">
+                    <span className="text-xs text-slate-400">down</span>
+                    <span>{truck.downPayment}</span>
+                  </div>
+                  <div className="flex justify-between items-end w-full">
+                    <span className="text-xs text-slate-700">Total</span>
+                    <span>{truck.listPrice + truck.downPayment}</span>
+                  </div>
+                </td>
+                <td className="px-2 py-2 text-xs">
+                  {truck.options.manufacture.map(
+                    ({ name }: { name: any }, key: any) => (
+                      <span
+                        key={key}
+                        className="bg-slate-50 px-2 py-1 rounded-lg"
+                      >
+                        {name}
+                      </span>
+                    ),
+                  )}
+                </td>
+                <td className="px-2 py-2 text-xs">
+                  {!truck.options.dealer ? (
+                    <span>None</span>
+                  ) : (
+                    truck.options.dealer.map(
+                      ({ name }: { name: any }, key: any) => (
+                        <span
+                          key={key}
+                          className="bg-slate-50 px-2 py-1 rounded-lg"
+                        >
+                          {name}
+                        </span>
+                      ),
+                    )
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
--- src/pages/_trucks/trucks1.ts
+++ src/pages/_trucks/trucks1.ts
@@ -11,24 +11,48 @@ const dealerDw = { dealer: "David Wilson", dealerShort: "dw" };
 const dealerFindlay = { dealer: "Findlay", dealerShort: "fn" };
 const dealerAutoNation = { dealer: "AutoNation", dealerShort: "an" };
 const dealerCc = { dealer: "Centennial Center", dealerShort: "cc" };
-const fullSpare = () => ({ name: "Full-Size Spare Tire" });
-const upgradeCold = () => ({ name: "SR5 Upgrade + Cold Weather Package" });
-const price = (srp: any, listPrice: any, downPayment: any) => ({
+const price = (
+  bsrp: any,
+  srp: any,
+  listPrice: any,
+  downPayment: any,
+  taxFees: any,
+) => ({
+  bsrp,
   srp,
   listPrice,
   downPayment,
+  taxFees,
 });
 const strickerDate = (date: any) => ({ stickerDate: date });
+
+const optsF = (...opts: any[]) =>
+  opts.reduce((acc, opt) => [...acc, opt()], []);
+
+const mFullSpare = () => ({ name: "Full-Size Spare Tire", price: 85 });
+const mUpgrdCld = () => ({ name: "SR5 Upgrade + Cold Weather Package" });
+const mfcSizeLed = {
+  name: "IP Side Accessory - LED Lantern",
+  price: 160,
+};
+
+/*
+2T. All Weather Floor Liners 199.00
+T6. Tailgate Insert - Black 99.00
+R2. Oval Tube Step - Black 600.00
+L5. Spray-On Bed Liner 575.00
+D5. Door Edge Guard 160.00
+ */
 export const trucks = [
   {
     vin: "3TMLB5JN1SM156384",
     ...SR5,
     ...exteriorColorWhite,
     ...dealerDw,
-    ...price(43863, 43863, 4887),
+    ...price(40490, 43863, 43863, 4887, 4303),
     ...strickerDate("2025-06-14"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: optsF(mFullSpare),
     },
   },
   {
@@ -36,10 +60,10 @@ export const trucks = [
     ...SR5,
     ...exteriorColorWhite,
     ...dealerFindlay,
-    ...price(44638, 43043, 4964),
+    ...price(40090, 43043, 44638, 4964, 4368),
     ...strickerDate("2025-07-05"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: [mFullSpare()],
     },
   },
   {
@@ -48,10 +72,10 @@ export const trucks = [
     ...SR5,
     ...exteriorColorWhite,
     ...dealerFindlay,
-    ...price(44038, 45633, 5064),
+    ...price(40490, 44038, 45633, 5064),
     ...strickerDate("2025-06-25"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: [mFullSpare()],
     },
   },
   {
@@ -59,7 +83,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorWhite,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -67,7 +91,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorWhite,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -75,7 +99,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorWhite,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -83,10 +107,10 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     ...dealerDw,
-    ...price(43544, 43544, 4855),
+    ...price(40490, 43544, 43544, 4855),
     ...strickerDate("2025-06-18"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: [mFullSpare()],
     },
   },
   {
@@ -94,10 +118,10 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     ...dealerFindlay,
-    ...price(43778, 45373, 5038),
+    ...price(40490, 43778, 45373, 5038),
     ...strickerDate("2025-07-18"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: [mFullSpare()],
     },
   },
   {
@@ -105,10 +129,10 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     ...dealerAutoNation,
-    ...price(43272, 45647, 5065),
+    ...price(40490, 43272, 45647, 5065),
     ...strickerDate("2025-08-01"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: [mFullSpare()],
     },
   },
   {
@@ -116,7 +140,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -124,7 +148,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -132,7 +156,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -140,7 +164,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -148,7 +172,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -156,7 +180,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -164,7 +188,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -172,10 +196,10 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     ...dealerCc,
-    ...price(47364, 47364, 2000),
+    ...price(40490, 47364, 47364, 2000),
     ...strickerDate("2025-08-01"),
     options: {
-      manufacture: [fullSpare()],
+      manufacture: [mFullSpare()],
     },
   },
   {
@@ -183,7 +207,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
   {
@@ -191,7 +215,7 @@ export const trucks = [
     ...SR5,
     ...exteriorColorSilver,
     options: {
-      manufacture: [fullSpare(), upgradeCold()],
+      manufacture: [mFullSpare(), mUpgrdCld()],
     },
   },
 ];
@@ -205,11 +229,15 @@ export const data = {
 export const truckFilter = (trucks: any) =>
   trucks.filter(
     (t: any) =>
-      t.options.manufacture.filter((mo: any) => mo.name === upgradeCold().name)
+      t.options.manufacture.filter((mo: any) => mo.name === mUpgrdCld().name)
         .length === 0,
   );
 
-export const skipCold = (options: any) => skipOption(upgradeCold, options);
+export const skipCold = (options: any) => skipOption(mUpgrdCld, options);
 export const skipOption = (option: any, options: any) => {
   return options.filter((o: any) => o.name === option);
 };
+
+/* Findlay Henderson
+ * 3TYKB5FN1ST43G934 trd sport for around 48K arriving October 31st.
+ */
